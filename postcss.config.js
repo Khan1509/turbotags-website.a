1234828aa5859897e1e2db9bb26224321828d5d8
Khan1509 <kaneez.9701001538@gmail.com>
2025-07-19 19:31:42 +0530

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    // Tailwind CSS is a PostCSS plugin
+    tailwindcss: {},
+    // Autoprefixer adds vendor prefixes to your CSS for broader browser compatibility
+    autoprefixer: {},
+  },
+};
