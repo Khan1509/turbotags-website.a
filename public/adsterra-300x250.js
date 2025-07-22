c5dd3f30b89c1974b0f06b3a15ab4f9edd601997
Khan1509 <kaneez.9701001538@gmail.com>
2025-07-22 19:07:45 +0530

    Create adsterra-300x250.js

---
+++ public/adsterra-300x250.js
@@ -0,0 +1,12 @@
+atOptions = {
+  'key' : '70357c016b9f7a90a4913531b29bb3b7',
+  'format' : 'iframe',
+  'height' : 250,
+  'width' : 300,
+  'params' : {}
+};
+
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.src = '//www.highperformanceformat.com/70357c016b9f7a90a4913531b29bb3b7/invoke.js';
+document.body.appendChild(s);
