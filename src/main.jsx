4462abd594ff4b247419749cd3b6307eeee5bd45
Khan1509 <kaneez.9701001538@gmail.com>
2025-09-09 00:33:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import '@fontsource/inter/400.css';
 import '@fontsource/inter/600.css';
 import '@fontsource/inter/800.css';
 
-ReactDOM.hydrateRoot(document.getElementById('root'),
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
