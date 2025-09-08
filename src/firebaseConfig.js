296490fa79082415761707f2577776623781b71e
Khan1509 <kaneez.9701001538@gmail.com>
2025-09-09 00:59:38 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,22 +1,29 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration is read from Vercel's Environment Variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCdQdG1cqcmUDwSSUlFVrP9arOV3qvdCpE",
-  authDomain: "turbotags-b4ca1.firebaseapp.com",
-  projectId: "turbotags-b4ca1",
-  storageBucket: "turbotags-b4ca1.appspot.com",
-  messagingSenderId: "766845880079",
-  appId: "1:766845880079:web:1722ceb1cf6e976231607a",
-  measurementId: "G-YXQPT1LPT8"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Analytics and get a reference to the service
-const analytics = getAnalytics(app);
 
-export { app, analytics };
+// Initialize Firebase services
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Export the services for use in other parts of your app
+export { app, analytics, auth, db, storage };
